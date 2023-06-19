46c6344fb88ed96e2ed1f6d16433d79830baff41
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-06-19 17:48:29 +1000

    basic app file structure set up

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from os import environ
+
 from flask import Flask
+from init import db, ma, bcrypt, jwt
+
+def create_app():
+    # create instance of Flask object
+    app = Flask(__name__)
+
+    # FLASK CONFIGURATION
+    # set database location with a DB URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+    # JWT secret key
+    app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET')
+
+    # Pass in app object to all instances of modules
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    @app.route('/')
+    def index():
+        return 'HI'
+    return app
+
---
+++ init.py
@@ -0,0 +1,13 @@
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+
+
+#  create instances of modules 
+db = SQLAlchemy()
+ma = Marshmallow()
+jwt = JWTManager()
+bcrypt = Bcrypt()
+
